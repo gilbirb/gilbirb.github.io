cd3e7d33efc050bab59f90acdc0460a8a44a19fa
GILBERT SEBASTIAN YOGIANA <z5641572@unsw.edu.au>
2026-05-11 14:06:44 +1000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gilbirb.github.io',
-  base: '/',
+  base: '/personal-site',
   vite: {
     plugins: [tailwindcss()]
   }
