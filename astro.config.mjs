b7fba4ecc9c28fdf07fdb8133bafaedef6ba6d66
GILBERT SEBASTIAN YOGIANA <z5641572@unsw.edu.au>
2026-06-02 14:30:59 +1000

    fixed latex math rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +12,8 @@ export default defineConfig({
     shikiConfig: {
       themes: { light: 'github-light', dark: 'github-dark' },
     },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
   vite: {
     plugins: [tailwindcss()]
